65940050da9b9018b42c5b75c7a095159e87af04
Inaiane Macedo Barros <inaiane.mbarros@gmail.com>
2025-12-02 21:02:27 -0300

    Corrigir propriedade do objeto

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const carrinho = [
+  { produtoId: 1, quantidade: 2 },
+  { produtoId: 2, quantidade: 1 },
+  { produtoId: 3, quantidade: 1 },
+];
+
+const mapCarrinho = new Map();
+
+mapCarrinho.set(1, carrinho[1]);
